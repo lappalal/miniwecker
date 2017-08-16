24204669fa99537e8868ee06365571ed100e7ea1
Jakob Sautner <jakob@sautner.cc>
2017-08-16 20:41:20 +0200

    switching this to a wordpress theme

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kath. Jugend Gauting - Homepage</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <header>
+      <div class="header">
+        <img src="img/header.svg" alt="dummy" class="header-img">
+      </div>
+    </header>
+
+    <nav>
+      <!--<img src="img/dummy.jpg" alt="dummy" class="featured-image">-->
+      <ul>
+        <li><a href="#" id="startseite">Startseite</a></li>
+        <li><a href="#" id="diekjg">Die KJG</a></li>
+        <li><a href="#" id="gruppen">Gruppen</a></li>
+        <li><a href="#" id="aktionen">Aktionen</a></li>
+        <li><a href="#" id="fotos">Fotos</a></li>
+        <li><a href="#" id="zeltlager">Zeltlager</a></li>
+        <li><a href="#" id="kontakt">Kontakt</a></li>
+      </ul>
+    </nav>
+
+    <main>
+      <div class="primary">
+        <div class="post">
+          <div class="date">
+            11 August 2017
+          </div>
+          <div class="title">
+            <h2>Neue Gruppenleiter 2017</h2>
+          </div>
+          <div class="featured-img">
+            <img src="img/leiter_2017.jpg" alt="Leiter 2017">
+          </div>
+          <div class="content">
+            Am 23. April wählte die Gruppenleiterrunde ihre neusten Mitglieder, die nach der Erstkommunion die Gruppenstunden für die jetztigen Drittklässler übernehmen. Wir freuen uns Miriam Hubel, Celina Janssen und Felicitas Sautner, sowie Tim Hager, Kilian Rettich und Luis Weberpals in der Gruppenleiterrunde begrüßen zu dürfen. Alle freuen sich schon sehr auf die erste Gruppenstunde und hoffen auf viele neue Grüpplinge. Unsere Neuen stellen sich auf dem Kommunionselternabend am 25. April im Pfarrheim vor und beantworten gerne eure Fragen. Die erste Gruppenstunde findet Freitag, 26. Mai bei den Jungs als auch bei den Mädchen um 15:30 Uhr statt!
+            Wir freuen uns auf zahlreiches kommen!
+          </div>
+          <div class="signature">
+            <h6>Eure KJG</h6>
+          </div>
+        </div>
+
+        <div class="post">
+          <div class="date">
+            11 August 2017
+          </div>
+          <div class="title">
+            <h2>Neue Gruppenleiter 2017</h2>
+          </div>
+          <div class="featured-img">
+            <img src="img/leiter_2017.jpg" alt="Leiter 2017">
+          </div>
+          <div class="content">
+            Am 23. April wählte die Gruppenleiterrunde ihre neusten Mitglieder, die nach der Erstkommunion die Gruppenstunden für die jetztigen Drittklässler übernehmen. Wir freuen uns Miriam Hubel, Celina Janssen und Felicitas Sautner, sowie Tim Hager, Kilian Rettich und Luis Weberpals in der Gruppenleiterrunde begrüßen zu dürfen. Alle freuen sich schon sehr auf die erste Gruppenstunde und hoffen auf viele neue Grüpplinge. Unsere Neuen stellen sich auf dem Kommunionselternabend am 25. April im Pfarrheim vor und beantworten gerne eure Fragen. Die erste Gruppenstunde findet Freitag, 26. Mai bei den Jungs als auch bei den Mädchen um 15:30 Uhr statt!
+            Wir freuen uns auf zahlreiches kommen!
+          </div>
+          <div class="signature">
+            <h6>Eure KJG</h6>
+          </div>
+        </div>
+
+        <div class="post">
+          <div class="date">
+            11 August 2017
+          </div>
+          <div class="title">
+            <h2>Neue Gruppenleiter 2017</h2>
+          </div>
+          <div class="featured-img">
+            <img src="img/leiter_2017.jpg" alt="Leiter 2017">
+          </div>
+          <div class="content">
+            Am 23. April wählte die Gruppenleiterrunde ihre neusten Mitglieder, die nach der Erstkommunion die Gruppenstunden für die jetztigen Drittklässler übernehmen. Wir freuen uns Miriam Hubel, Celina Janssen und Felicitas Sautner, sowie Tim Hager, Kilian Rettich und Luis Weberpals in der Gruppenleiterrunde begrüßen zu dürfen. Alle freuen sich schon sehr auf die erste Gruppenstunde und hoffen auf viele neue Grüpplinge. Unsere Neuen stellen sich auf dem Kommunionselternabend am 25. April im Pfarrheim vor und beantworten gerne eure Fragen. Die erste Gruppenstunde findet Freitag, 26. Mai bei den Jungs als auch bei den Mädchen um 15:30 Uhr statt!
+            Wir freuen uns auf zahlreiches kommen!
+          </div>
+          <div class="signature">
+            <h6>Eure KJG</h6>
+          </div>
+        </div>
+      </div>
+
+      <div class="secondary">
+        <div class="widget">
+          <div class="title">
+            SIDEBAR
+          </div>
+          <div class="content">
+            Am 23. April wählte die Gruppenleiterrunde ihre neusten Mitglieder, die nach der Erstkommunion die Gruppenstunden für die jetztigen Drittklässler übernehmen. Wir freuen uns Miriam Hubel, Celina Janssen und Felicitas Sautner, sowie Tim Hager, Kilian Rettich und Luis Weberpals in der Gruppenleiterrunde begrüßen zu dürfen. Alle freuen sich schon sehr auf die erste Gruppenstunde und hoffen auf viele neue Grüpplinge. Unsere Neuen stellen sich auf dem Kommunionselternabend am 25. April im Pfarrheim vor und beantworten gerne eure Fragen. Die erste Gruppenstunde findet Freitag, 26. Mai bei den Jungs als auch bei den Mädchen um 15:30 Uhr statt!
+            Wir freuen uns auf zahlreiches kommen!
+          </div>
+        </div>
+      </div>
+    </main>
+
+  </body>
+</html>
