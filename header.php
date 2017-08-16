8bf92b3a1d3658f0c74a13b64006d3a8ccf756c1
Jakob Sautner <jakob@sautner.cc>
2017-08-16 20:48:57 +0200

    wip

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Kath. Jugend Gauting - Homepage</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <header>
+      <div class="header">
+        <img src="img/header.svg" alt="dummy" class="header-img">
+      </div>
+    </header>
+
+    <nav>
+      <!--<img src="img/dummy.jpg" alt="dummy" class="featured-image">-->
+      <ul>
+        <li><a href="#" id="startseite">Startseite</a></li>
+        <li><a href="#" id="diekjg">Die KJG</a></li>
+        <li><a href="#" id="gruppen">Gruppen</a></li>
+        <li><a href="#" id="aktionen">Aktionen</a></li>
+        <li><a href="#" id="fotos">Fotos</a></li>
+        <li><a href="#" id="zeltlager">Zeltlager</a></li>
+        <li><a href="#" id="kontakt">Kontakt</a></li>
+      </ul>
+    </nav>
